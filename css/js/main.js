0845f69b576c4a3ba298a10cefc252b52ec4fdc9
royceshen <royceshen1126@gmail.com>
2017-03-04 08:39:51 +0800

    11

--- css/js/main.js
+++ css/js/main.js
@@ -9,13 +9,13 @@
 	function sidebar_show()
 	{
 		mask.fadeIn();
-		sidebar.css('right',0);
+		sidebar.css('left',0);
 	}
 
 	function sidebar_hide()
 	{
 		mask.fadeOut();
-		sidebar.css('right', -sidebar.width())
+		sidebar.css('left', -sidebar.width())
 	}
 	sidebar_trigger.on('click',sidebar_show)
 	mask.on('click',sidebar_hide)
